f21702d3317c74ba56ca3ceab0dc008858a3472e
Andreas Ulrich <andreas@ulrich.com.de>
2026-03-26 09:44:34 +0100

    feat: add Linux icon setup and portable launcher assets

--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+//go:embed build/appicon.png
+var appIcon []byte
 
 func main() {
 	// Create an instance of the app structure
@@ -25,6 +29,10 @@ func main() {
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
+		Linux: &linux.Options{
+			Icon:        appIcon,
+			ProgramName: "mdit",
+		},
 		Bind: []interface{}{
 			app,
 		},
